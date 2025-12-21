b3e9056e64ac64c3241efd3669e3968834e9a4ba
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-21 11:50:49 +0500

    Add Profile Components Export. Create an Index File to Export various Profile-Related Components, Including Followers, Tabs, Header, and Projects, Enhancing Modularity and Ease of Access for Profile Functionalities

---
+++ src/components/profile/index.ts
@@ -0,0 +1,9 @@
+// <== PROFILE COMPONENTS ==>
+export {
+  ProfileFollowers,
+  ProfileFollowing,
+  ProfileFollowersSkeleton,
+} from "./profile-followers";
+export { ProfileTabs, ProfileTabContent } from "./profile-tabs";
+export { ProfileHeader, ProfileHeaderSkeleton } from "./profile-header";
+export { ProfileProjects, ProfileProjectsSkeleton } from "./profile-projects";
