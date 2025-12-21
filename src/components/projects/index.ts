d56a02741e8601979a7f7e172df7545bacb33410
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-21 19:47:11 +0500

    Add Project Components Index File. Create an Index File to Export Various Project-Related Components, Streamlining Imports and Enhancing Modularity in the Project Structure

---
+++ src/components/projects/index.ts
@@ -0,0 +1,9 @@
+// <== PROJECT COMPONENTS EXPORTS ==>
+export * from "./project-card";
+export * from "./project-grid";
+export * from "./upvote-button";
+export * from "./project-header";
+export * from "./project-gallery";
+export * from "./project-filters";
+export * from "./bookmark-button";
+export { ProjectForm } from "./project-form";
