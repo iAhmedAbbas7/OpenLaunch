7ab45be0588d6f57b51d1415341a764300adb544
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-21 11:56:27 +0500

    Add Social Components Export. Create an Index File to Export FollowButton, UserHoverCard, and UserCard Components, Enhancing Modularity and Accessibility for Social Features

---
+++ src/components/social/index.ts
@@ -0,0 +1,4 @@
+// <== SOCIAL COMPONENTS ==>
+export { FollowButton } from "./follow-button";
+export { UserHoverCard } from "./user-hover-card";
+export { UserCard, UserCardSkeleton } from "./user-card";
