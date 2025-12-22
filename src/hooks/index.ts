cc70a95f8566d075f6919a8e11d2eb6ff3c59d4b
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-22 23:54:50 +0500

    Add GitHub Hook Export. Include the New use-github Hook in the Hooks Index to Enhance Functionality and Integration with GitHub Features

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 // <== HOOKS ==>
 export * from "./use-auth";
 export * from "./use-follow";
+export * from "./use-github";
 export * from "./use-profile";
 export * from "./use-projects";
 export * from "./use-debounce";
