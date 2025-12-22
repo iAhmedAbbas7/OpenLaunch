1d12dea76e505b919f444a31fdb37b45dde32f4f
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-22 23:52:54 +0500

    Add Code Browser Exports. Introduce an Index File for the Code Browser Component to Streamline Exports of FileTree, SyntaxHighlighter, FileViewer, and CodeBrowser Components, Enhancing Modularity and Organization

---
+++ src/components/projects/code-browser/index.ts
@@ -0,0 +1,5 @@
+// <== CODE BROWSER EXPORTS ==>
+export { FileTree, FileTreeSkeleton } from "./file-tree";
+export { SyntaxHighlighter } from "./syntax-highlighter";
+export { FileViewer, FileViewerSkeleton } from "./file-viewer";
+export { CodeBrowser, CodeBrowserSkeleton } from "./code-browser";
