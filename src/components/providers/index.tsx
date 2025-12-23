de81cab4eea8a5caddf28409e9f91cd559df5846
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-23 23:53:00 +0500

    Refactor Providers Component. Introduce Themed Toaster for Notifications, Enhancing User Experience with Contextual Theme Support and Improved Positioning

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -2,8 +2,9 @@
 "use client";
 
 // <== IMPORTS ==>
-import { Toaster } from "sonner";
+import { useTheme } from "next-themes";
 import { AuthProvider } from "./auth-provider";
+import { Toaster } from "@/components/ui/sonner";
 import { QueryProvider } from "./query-provider";
 import { ThemeProvider } from "./theme-provider";
 
@@ -26,26 +27,34 @@ export const Providers = ({ children }: ProvidersProps) => {
           {/* CHILDREN */}
           {children}
           {/* TOASTER FOR NOTIFICATIONS */}
-          <Toaster
-            position="bottom-right"
-            toastOptions={{
-              className: "font-sans",
-              classNames: {
-                success:
-                  "bg-emerald-500/10 border-emerald-500/20 text-emerald-500",
-                error:
-                  "bg-destructive/10 border-destructive/20 text-destructive",
-                info: "bg-primary/10 border-primary/20 text-primary",
-                warning: "bg-amber-500/10 border-amber-500/20 text-amber-500",
-              },
-            }}
-            richColors
-            closeButton
-            expand={false}
-          />
+          <ToasterWithTheme />
         </AuthProvider>
       </ThemeProvider>
     </QueryProvider>
+  );
+};
+
+// <== TOASTER WITH THEME ==>
+const ToasterWithTheme = () => {
+  // GET THEME
+  const { resolvedTheme } = useTheme();
+  // RETURNING TOASTER COMPONENT
+  return (
+    <Toaster
+      position="top-right"
+      theme={resolvedTheme === "dark" ? "dark" : "light"}
+      toastOptions={{
+        className: "font-sans",
+        classNames: {
+          success: "bg-emerald-500/10 border-emerald-500/20 text-emerald-500",
+          error: "bg-destructive/10 border-destructive/20 text-destructive",
+          info: "bg-primary/10 border-primary/20 text-primary",
+          warning: "bg-amber-500/10 border-amber-500/20 text-amber-500",
+        },
+      }}
+      richColors
+      expand={false}
+    />
   );
 };
 
