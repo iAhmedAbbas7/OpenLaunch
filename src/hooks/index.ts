386be1f5eb18f1f9aa42f4869624ca4cadb9ceea
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-24 17:16:04 +0500

    Add Articles Hook Export. Include the use-articles Hook in the Index File to Streamline Imports and Enhance Code Organization for Article-Related Functionalities

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,7 @@ export * from "./use-auth";
 export * from "./use-follow";
 export * from "./use-github";
 export * from "./use-profile";
+export * from "./use-articles";
 export * from "./use-projects";
 export * from "./use-debounce";
 export * from "./use-categories";
