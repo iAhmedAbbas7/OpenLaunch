663a7d5c5170eec2872ffd1ce99f1da6b35eed15
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-24 17:14:13 +0500

    Add Article Components Index. Create a New Index File to Export Various Article-Related Components, Streamlining Imports and Enhancing Code Organization

---
+++ src/components/articles/index.ts
@@ -0,0 +1,36 @@
+// <== EXPORT ARTICLE COMPONENTS ==>
+
+// <== ARTICLE EDITOR COMPONENTS ==>
+export {
+  ArticleEditorForm,
+  RichTextEditor,
+  TagInput,
+  CoverImageUpload,
+  PublishDialog,
+  type SeoSettings,
+} from "./editor";
+
+// <== ARTICLE ACTIONS ==>
+export {
+  LikeButton,
+  BookmarkButton,
+  ArticleActionsBar,
+} from "./article-actions";
+
+// <== READING PROGRESS ==>
+export { ReadingProgress } from "./reading-progress";
+
+// <== ARTICLE CARD ==>
+export { ArticleCard, ArticleCardSkeleton } from "./article-card";
+
+// <== ARTICLE HEADER ==>
+export { ArticleHeader, ArticleHeaderSkeleton } from "./article-header";
+
+// <== ARTICLE CONTENT ==>
+export { ArticleContent, ArticleContentSkeleton } from "./article-content";
+
+// <== ARTICLE FILTERS ==>
+export { ArticleFilters, ArticleFiltersSkeleton } from "./article-filters";
+
+// <== TABLE OF CONTENTS ==>
+export { TableOfContents, TableOfContentsSkeleton } from "./table-of-contents";
