36951e631ab0fb48e4cc750634b7efb9ba019238
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-24 23:15:16 +0500

    Add Comments Component Exports for Modular Structure. Create an Index File to Streamline the Export of Various Comment-Related Components, Enhancing Code Organization and Accessibility in the Comments Module

---
+++ src/components/comments/index.ts
@@ -0,0 +1,11 @@
+// <== COMMENTS COMPONENTS EXPORTS ==>
+export {
+  CommentSkeleton,
+  CommentsListSkeleton,
+  CommentFormSkeleton,
+} from "./comment-skeleton";
+export { CommentForm } from "./comment-form";
+export { CommentItem } from "./comment-item";
+export { CommentUpvote } from "./comment-upvote";
+export { CommentReplies } from "./comment-replies";
+export { CommentsSection } from "./comments-section";
