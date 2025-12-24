5a21d3f706ca0ebfd7cb52db14633f781e974e1a
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-24 17:14:57 +0500

    Add Article Editor Components Index. Create a New Index File to Export Various Article Editor-Related Components, Streamlining Imports and Enhancing Code Organization

---
+++ src/components/articles/editor/index.ts
@@ -0,0 +1,16 @@
+// <== ARTICLE EDITOR COMPONENTS ==>
+
+// <== TAG INPUT ==>
+export { TagInput } from "./tag-input";
+
+// <== RICH TEXT EDITOR ==>
+export { RichTextEditor } from "./rich-text-editor";
+
+// <== COVER IMAGE UPLOAD ==>
+export { CoverImageUpload } from "./cover-image-upload";
+
+// <== ARTICLE EDITOR FORM ==>
+export { ArticleEditorForm } from "./article-editor-form";
+
+// <== PUBLISH DIALOG ==>
+export { PublishDialog, type SeoSettings } from "./publish-dialog";
