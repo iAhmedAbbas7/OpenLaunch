509c349e6ee308b09ab690e01f5002698ebac5a0
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-26 23:15:55 +0500

    Add Messaging Components Index File for Improved Structure. This Commit Introduces an Index File for the Messaging Components, Streamlining the Export of Various Components such as User Search, Message Item, and Conversation-Related Components, thereby Enhancing the Organization and Accessibility of the Messaging Module

---
+++ src/components/messaging/index.ts
@@ -0,0 +1,8 @@
+// <== MESSAGING COMPONENTS ==>
+export * from "./user-search";
+export * from "./message-item";
+export * from "./message-input";
+export * from "./conversation-item";
+export * from "./conversation-list";
+export * from "./conversation-thread";
+export * from "./new-conversation-dialog";
