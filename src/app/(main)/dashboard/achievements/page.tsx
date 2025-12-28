db378d86a8d71b1bac42018a77b3f13aff1214a0
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-28 22:01:34 +0500

    Implement User Authentication Check in MyAchievementsPage. This Update Introduces a Supabase Client to Verify User Authentication, Redirecting Unauthenticated Users to the Sign-In Page, thereby Enhancing Security and User Experience on the Achievements Dashboard

--- src/app/(main)/dashboard/achievements/page.tsx
+++ src/app/(main)/dashboard/achievements/page.tsx
@@ -1,5 +1,7 @@
 // <== IMPORTS ==>
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
 import { AchievementsClient } from "./achievements-client";
 
 // <== METADATA ==>
@@ -11,7 +13,18 @@ export const metadata: Metadata = {
 };
 
 // <== MY ACHIEVEMENTS PAGE ==>
-const MyAchievementsPage = () => {
+const MyAchievementsPage = async () => {
+  // CREATE SUPABASE CLIENT
+  const supabase = await createClient();
+  // GET USER
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  // IF NO USER, REDIRECT TO SIGN IN
+  if (!user) {
+    // REDIRECT TO SIGN IN
+    redirect("/sign-in");
+  }
   // RETURNING ACHIEVEMENTS CLIENT
   return <AchievementsClient />;
 };
