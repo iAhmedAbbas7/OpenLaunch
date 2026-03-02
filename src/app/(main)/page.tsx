48fe83c1573ad451273e3e0eb69922caea598f7f
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-03-02 14:31:16 +0500

    Add Suspense Wrapper to HomePage Component. This Update Introduces a Suspense Fallback around the HomePageClient, Enhancing Loading Behavior and User Experience during Page Rendering

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,15 @@
 // <== IMPORTS ==>
+import { Suspense } from "react";
 import { HomePageClient } from "./home-page-client";
 
 // <== HOME PAGE ==>
 const HomePage = () => {
   // RETURNING HOME PAGE
-  return <HomePageClient />;
+  return (
+    <Suspense fallback={<div />}>
+      <HomePageClient />
+    </Suspense>
+  );
 };
 
 // <== EXPORTING HOME PAGE ==>
